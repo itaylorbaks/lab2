2344c885cec0c13a937557ca392d3212e8c11819
itaylorbaks <iyobaks@gmail.com>
2026-09-13 23:02:33 +0000

    up to here, working on nosuchelement

--- csci151fa26-lab-2-oop-taylorbakare-main/Arrays.java
+++ csci151fa26-lab-2-oop-taylorbakare-main/Arrays.java
@@ -4,6 +4,9 @@
  * @author Your Name Goes Here
  */
 
+import java.util.Scanner;
+import java.util.InputMismatchException; 
+
 public class Arrays {
      /**
      * The main function is the implementation of our program.
@@ -11,7 +14,21 @@ public class Arrays {
      * @param args The command line arguments, unused
      */
     public static void main(String[] args) {
-        // TODO: implement your solution here
+        System.out.println("How many numbers do you want in your array?: ");
+        Scanner numlength = new Scanner(System.in);
+        int[] arr = new int[numlength.nextInt()];
+        numlength.close();
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Number " + i + " value: ");
+           Scanner numval = new Scanner(System.in);
+           arr[i] = numval.nextInt();
+           numval.close();
+        }
+        System.out.println("Done!");
+        // for (int i = 0; i < arr.length; i++) {
+        //    System.out.println(arr[i]);
+        // }
+        
     }
 
 }
